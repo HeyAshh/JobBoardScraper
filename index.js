4f3084285b7be11221f62ccf053bb28ef6c28213
Ashh <Heyashh1@gmail.com>
2020-09-27 01:11:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-  const { GoogleSpreadsheet } = require('google-spreadsheet');
-  
-  // spreadsheet key is the long id in the sheets URL
-  const doc = new GoogleSpreadsheet('************************');
+const Sheet = require('./sheet')
+const fetch = require('node-fetch');
 
-  (async function() {
-
-
-  console.log(doc.title);
-  await doc.updateProperties({ title: 'renamed doc' });
-  
-  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-  await sheet.addRows([
-      { Title: 'Software Engineer', Location: 'SF' },
-      { Title: 'Designger', Location: 'NY' },
-    ]);
-})()
+(async function() {
+  const sheet = new Sheet();
+  await sheet.load();
+})
