2235bd0fbf508384ae7a56c22c1802ab1667f931
Ashh <Heyashh1@gmail.com>
2020-09-29 01:01:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const fetch = require('node-fetch');
 (async function() {
   const sheet = new Sheet();
   await sheet.load();
+
+  fetch('https://jobs.github.com/positions.json?description=python&location=new+york');
 })
