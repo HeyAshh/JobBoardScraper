4a728db6510f242b4ffac6b2e3d6e06141452130
Ashh <Heyashh1@gmail.com>
2020-10-01 01:05:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const Sheet = require('./sheet')
 const fetch = require('node-fetch');
 
 (async function() {
+  
+  const res = await fetch('https://jobs.github.com/positions.json?location=remote');
+  const json = await res.json();
+
+  const rows = json.map(job => {
+    return {
+      Company: job.Company,
+      Title: job.Title,
+      Location: job.Location,
+      Date: job.created_at,
+      url: job.url
+    }
+  })
+
   const sheet = new Sheet();
   await sheet.load();
 
-  const res = await fetch('https://jobs.github.com/positions.json?description=python&location=new+york');
-  const json = await res.json();
+  await sheet.addRows(rows);
 
   console.log({json});
 })()
